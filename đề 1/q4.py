fc24785d71b149cf3da31bc873333eec5427be2b
hotonbao <htbqn2003@gmail.com>
2024-04-26 08:43:33 +0700

    fix đề 1 - Q4

--- đề 1/q4.py
+++ đề 1/q4.py
@@ -1,7 +1,7 @@
 # merge first name and last name as single token in sentence
 #input: textt =" Robert Langdon is a famous character in Ho Ton Bao various novels."
 #output: "Robert Langdon", "is","a", "famous", character","Ho Ton",  "in", "various", "novels"
-
+import re
 def merge_name(text):
     text = text.split()
     for i in range(len(text)-1):
@@ -10,7 +10,26 @@ def merge_name(text):
             text.pop(i+1)
     return text
 
-text = "Robert Langdon is a famous character in various novels."
-tokens = merge_name(text)
-for token in tokens:
-    print(token)
+def tokenize(text):
+    text = text.split()
+    one_token = ""
+    array = []
+    for token in text:
+        if token.istitle():
+            if one_token =="":
+                one_token += token
+
+            else:
+                one_token += " " + token
+        else:
+            if(one_token!=""):
+                array.append(one_token)
+            one_token = ""
+            array.append(token)
+    return array
+
+text = "Robert Langdon is a famous character Abao Xom Tieu in various novels."
+
+
+token = tokenize(text)
+print("token: ", token)
